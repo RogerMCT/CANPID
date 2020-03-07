f4cbabb4639202772b53b77a703d0cbbb1b500db
RogerMCT <rogermct@hotmail.com>
2020-03-07 16:18:17 -0500

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,34 +1,20 @@
 package frc.robot;
 
-import com.revrobotics.CANEncoder;
 import com.revrobotics.CANPIDController;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.ControlType;
-import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
   private final CANSparkMax shooterLeftMasterMotor = new CANSparkMax(8, MotorType.kBrushless);
-  private final CANSparkMax shooterRightSlaveMotor = new CANSparkMax(7, MotorType.kBrushless);
   
   private CANPIDController shooterPIDController;
 
   @Override
   public void robotInit() {
-    shooterLeftMasterMotor.restoreFactoryDefaults();
-    shooterLeftMasterMotor.setSmartCurrentLimit(38);
     shooterLeftMasterMotor.setInverted(true);
-    shooterLeftMasterMotor.setIdleMode(IdleMode.kCoast);
-    shooterLeftMasterMotor.burnFlash();
-
-    shooterRightSlaveMotor.restoreFactoryDefaults();
-    shooterRightSlaveMotor.setSmartCurrentLimit(38);
-    shooterRightSlaveMotor.setIdleMode(IdleMode.kCoast);
-    shooterRightSlaveMotor.follow(shooterLeftMasterMotor, true);
-    shooterRightSlaveMotor.burnFlash();
-
     shooterPIDController = shooterLeftMasterMotor.getPIDController();
     shooterPIDController.setP(0);
     shooterPIDController.setI(0);
